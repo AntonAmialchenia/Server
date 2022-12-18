934e316edd807d5a82ae107e8a000c281df249dd
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2022-12-18 19:35:55 +0300

    Запросы
    GET
    POST
    DELETE
    PUT

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,85 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  const a = 4;
-  if (a > 5) {
-    res.send('ok!');
-  } else {
-    res.send('Hello World!');
+const HTTP_STATUSES = {
+  OK_200: 200,
+  CREATED_201: 201,
+  NO_CONTENT_204:204,
+
+  BAD_REQUEST_400: 400,
+  NOT_FOUND_404: 404,
+}
+
+const jsonBodyMiddleware = express.json();
+app.use(jsonBodyMiddleware);
+
+const db = {
+  courses: [
+    { id: 1, title: 'front-end' },
+    { id: 2, title: 'back-end' },
+    { id: 3, title: 'automation qa' },
+    { id: 4, title: 'devops' },
+  ],
+};
+
+app.get('/corses', (req, res) => {
+  let foundCourses = db.courses;
+  if (req.query.title) {
+    foundCourses = foundCourses.filter(
+      (c) => c.title.indexOf(req.query.title as string) > -1
+    );
   }
+
+  res.json(foundCourses);
+});
+
+app.post('/corses', (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+
+  const createdCourse = {
+    id: +new Date(),
+    title: req.body.title,
+  };
+  db.courses.push(createdCourse);
+  res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
+});
+app.get('/corses/:id', (req, res) => {
+  const foundCorse = db.courses.find((c) => c.id === +req.params.id);
+
+  if (!foundCorse) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+  res.json(foundCorse);
+});
+
+app.delete('/corses/:id', (req, res) => {
+  db.courses = db.courses.filter(c => c.id !== +req.params.id);
+
+  
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+
+app.put('/corses/:id', (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+    return;
+  }
+  const foundCorse = db.courses.find(c => c.id === +req.params.id);
+
+  if (!foundCorse) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+
+  foundCorse.title = req.body.title;
+
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
 });
 
 app.get('/samurai', (req, res) => {
