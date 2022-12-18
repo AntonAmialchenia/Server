6f98c42d86eb6f1589a0ebd755167b50ecff68cf
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2022-12-18 16:11:56 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const a = 4;
+  if (a > 5) {
+    res.send('ok!');
+  } else {
+    res.send('Hello World!');
+  }
+});
+
+app.get('/samurai', (req, res) => {
+  res.send('Hello Anton!!!!!');
+});
+
+app.post('/samurai', (req, res) => {
+  res.send('we have created samurai');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
